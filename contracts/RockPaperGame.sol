c9a35f686f2056224cef300e23c7f28bc734c675
Dmitry <JijaLoqie>
2023-05-07 22:50:25 +0300

    add moddifier

--- contracts/RockPaperGame.sol
+++ contracts/RockPaperGame.sol
@@ -9,33 +9,44 @@ contract RockPaperGame {
         Scissors
     }
 
+    event moveCommited(bytes32 _hashedMove, uint time);
+    event moveRevealed(address who, Element move, uint time);
+    event gameEnded(uint time, address game);
+
     mapping (address => bytes32) public elements;
     mapping (address => Element) public results;
     address[] public participants;
     bool public gameStopped;
 
-    // function, where input - hashed bytes32, which includes secret word, sender and his move
-    function commitMove(bytes32 _hashedMove) external {
+    modifier gameIsRelevant {
         require(!gameStopped, "Game is stopped!");
+        _;
+    }
+
+    // function, where input - hashed bytes32, which includes secret word, sender and his move
+    function commitMove(bytes32 _hashedMove) external gameIsRelevant() {
         require(elements[msg.sender] == bytes32(0), "Move is already commited!");
-        require(participants.length < 3, "Game is full of participants!");
+        require(participants.length < 2, "Game is full of participants!");
 
         participants.push(msg.sender);
         elements[msg.sender] = _hashedMove;
+
+        emit moveCommited(_hashedMove, block.timestamp);
     }
 
     // function, created for testing, is not used for commit/reveal pattern
-    function commitMove(Element _move, string memory _secret) external {
+    function commitMove(Element _move, string memory _secret) external gameIsRelevant() {
         bytes32 _hashedMove = keccak256(abi.encodePacked(_move, _secret, msg.sender));
 
         require(_move != Element.BadElement, "bad element type!");
 
-        require(!gameStopped, "Game is stopped!");
         require(elements[msg.sender] == bytes32(0), "Move is already commited!");
-        require(participants.length < 3, "Game is full of participants!");
+        require(participants.length < 2, "Game is full of participants!");
 
         participants.push(msg.sender);
         elements[msg.sender] = _hashedMove;
+
+        emit moveCommited(_hashedMove, block.timestamp);
     }
 
     function revealMove(Element _move, string memory _secret) external {
@@ -46,23 +57,46 @@ contract RockPaperGame {
 
         delete elements[msg.sender];
         results[msg.sender] = _move;
+
+        emit moveRevealed(msg.sender, _move, block.timestamp);
     }
 
-    function stopGame() external {
-        require(participants.length == 3, "Not enough participants! Need 3");
+    function stopGame() external gameIsRelevant() {
+        require(participants.length == 2, "Not enough participants! Need 2");
+
         gameStopped = true;
+
+
+        emit gameEnded(block.timestamp, address(this));
     }
 
-    function getWinner() public view returns(Element[3] memory){
+
+    function getResult() public view returns(Element[2] memory){
         require(gameStopped,  "Wait for game stop!!");
         
         Element move1 = results[participants[0]];
-        require(move1 != Element.BadElement, "1st player should reveal!");
+        require(move1 != Element.BadElement, "1-st player should reveal!");
         Element move2 = results[participants[1]];
-        require(move2 != Element.BadElement, "2st player should reveal!");
-        Element move3 = results[participants[2]];
-        require(move3 != Element.BadElement, "3st player should reveal!");
+        require(move2 != Element.BadElement, "2-st player should reveal!");
 
-        return [move1, move2, move3];        
+
+        return [move1, move2];
+    }
+
+    function getWinner() public view returns(address){
+        Element[2] memory result = getResult();
+
+        if (result[0] == result[1]) {
+            return address(0);
+        }
+
+        if (result[0] == Element.Rock && result[1] == Element.Paper ||
+        result[0] == Element.Paper && result[1] == Element.Scissors ||
+        result[0] == Element.Scissors && result[1] == Element.Rock) {
+
+            return participants[1];
+        } else {
+            return participants[0];
+        }
     }
 }
