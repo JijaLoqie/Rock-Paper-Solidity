294fd1a1a79a9ccbf0dea04c1ca565c47cc8219c
JijaLoqie <dmitry.getrandompost@gmail.com>
2023-05-13 20:36:55 +0300

    almost done

--- contracts/RockPaperGame.sol
+++ contracts/RockPaperGame.sol
@@ -1,22 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-enum Element {
-    BadElement,
-    Rock,
-    Paper,
-    Scissors
-}
 
 contract RockPaperGame {
 
     event moveCommited(bytes32 _hashedMove, uint time);
-    event moveRevealed(address who, Element move, uint time);
+    event moveRevealed(address who, uint move, uint time);
     event gameEnded(uint time, address game);
 
     mapping (address => bytes32) public elements;
-    mapping (address => Element) public results;
-    address[] public participants;
+    mapping (address => uint) public results;
+    address[] private participants;
     bool public gameStopped;
 
     modifier gameIsRelevant {
@@ -24,21 +18,30 @@ contract RockPaperGame {
         _;
     }
 
+    function getParticipants() external view returns(address[] memory) {
+		return participants;
+	}
+    function callCommitMove(uint _move, string memory _secret) external gameIsRelevant {
+        bytes32 _hashedMove = keccak256(abi.encodePacked(msg.sender, _move, _secret));
+
+        require(_move != 0, "bad element type!");
+        commitMove(msg.sender, _hashedMove);
+    }
     // function, where input - hashed bytes32, which includes secret word, sender and his move
-    function commitMove(bytes32 _hashedMove) external gameIsRelevant {
-        require(elements[msg.sender] == bytes32(0), "Move is already commited!");
+    function commitMove(address who, bytes32 _hashedMove) public gameIsRelevant {
+        require(elements[who] == bytes32(0), "Move is already commited!");
         require(participants.length < 2, "Game is full of participants!");
 
-        participants.push(msg.sender);
-        elements[msg.sender] = _hashedMove;
+        participants.push(who);
+        elements[who] = _hashedMove;
 
         emit moveCommited(_hashedMove, block.timestamp);
     }
 
-    function revealMove(Element _move, string memory _secret) external gameIsRelevant {
+    function revealMove(uint _move, string memory _secret) external {
         require(gameStopped, "Wait for game stop!!");
 
-        bytes32 hashedMove = keccak256(abi.encodePacked(_move, _secret, msg.sender));
+        bytes32 hashedMove = keccak256(abi.encodePacked(msg.sender, _move, _secret));
         require(hashedMove == elements[msg.sender], "Wrong reveal proof!");
 
         delete elements[msg.sender];
@@ -57,13 +60,13 @@ contract RockPaperGame {
     }
 
 
-    function getResult() public view returns(Element[2] memory){
+    function getResult() public view returns(uint[2] memory){
         require(gameStopped,  "Wait for game stop!!");
         
-        Element move1 = results[participants[0]];
-        require(move1 != Element.BadElement, "1-st player should reveal!");
-        Element move2 = results[participants[1]];
-        require(move2 != Element.BadElement, "2-st player should reveal!");
+        uint move1 = results[participants[0]];
+        require(move1 != 0, "1-st player should reveal!");
+        uint move2 = results[participants[1]];
+        require(move2 != 0, "2-st player should reveal!");
 
 
         return [move1, move2];
--- contracts/RockPaperGameAdapter.sol
+++ contracts/RockPaperGameAdapter.sol
@@ -1,36 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./RockpaperGame.sol";
+import "./RockPaperGame.sol";
 
 contract RockPaperGameAdapter {
     address rockPaperGame;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier shouldBeInit() {
+        require(rockPaperGame != address(0), "rockPaperGame should be init!!!");
+        _;
+    }
+    modifier onlyOwner() {
+        require(owner == msg.sender, "only owner of this contract can do that!!!");
+        _;
+    }
+    function init(address _rockPaperGame) onlyOwner public {
+        rockPaperGame = _rockPaperGame;
+    }
 
         // function, created for testing, is not used for commit/reveal pattern
-    function commitMove(Element _move, string memory _secret) external {
-        bytes32 _hashedMove = keccak256(abi.encodePacked(_move, _secret, msg.sender));
+    function callCommitMove(uint _move, string memory _secret) external onlyOwner shouldBeInit {
+        bytes32 _hashedMove = keccak256(abi.encodePacked(msg.sender, _move, _secret));
 
-        require(_move != Element.BadElement, "bad element type!");
-        (bool success,) = rockPaperGame.call("commitMove");
+        require(_move != 0, "bad element type!");
+        (bool success,) = rockPaperGame.call(
+			abi.encodeWithSignature("commitMove(address,bytes32)", owner, _hashedMove)
+		);
+
+		require(success, "Something went wrong!");
     }
 
     
 
-    function getWinner() public view returns(address){
-        (bool successGetResult, bytes memory result) = rockPaperGame.call("getResult");
-        (bool successGetParticipants, bytes memory participants) = rockPaperGame.call("participants");
+    function GetWinner() external onlyOwner shouldBeInit returns(address) {
+        (bool successGetResult, bytes memory _result) = rockPaperGame.call(
+			abi.encodeWithSignature("getResult(bytes32)")
+		);
+		require(successGetResult, "getResult: something went wrong!");
 
+        (bool successGetParticipants, bytes memory _participants) = rockPaperGame.call(
+			abi.encodeWithSignature("getParticipants()")
+			);
+		require(successGetParticipants, "getParticipants: something went wrong!");
+
+		address[2] memory participants = abi.decode(_participants, (address[2]));
+		uint[2] memory result = abi.decode(_result, (uint[2]));
 
         if (result[0] == result[1]) {
             return address(0);
         }
-        // SOLUTION: 
-        // 1. cast `result` to Element[2]
-        // 2. cast `participants` to address[2]
 
-        if (result[0] == Element.Rock && result[1] == Element.Paper ||
-        result[0] == Element.Paper && result[1] == Element.Scissors ||
-        result[0] == Element.Scissors && result[1] == Element.Rock) {
+        if (result[0] == 1 && result[1] == 2 ||
+        result[0] == 2 && result[1] == 3 ||
+        result[0] == 3 && result[1] == 1) {
 
             return participants[1];
         } else {
